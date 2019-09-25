460ce6f8f0179fe8f81fa66e542f4b1b14a437ff
ANDRÉ JOÃO CORRÊA <andre99.correa@catolicasc.edu.br>
2019-09-24 21:46:55 -0300

    File

--- services/postsService.js
+++ services/postsService.js
@@ -1,40 +1,22 @@
+var fs = require('fs');
 
-var getPosts = function() {
-  var posts = [
-    {
-      id: 1,
-      title: 'Posts 1',
-      image: 'post1.jpg',
-      description: 'Primeiro Trabalho',
-      body: 'Lista Posts e Projetos'
-    },
-    {
-      id: 2,
-      title: 'Post 2',
-      image: 'post7.jpg',
-      description: 'engenharia software',
-      body: 'Meu segundo post bla bla'
-    },
-    {
-      id: 3,
-      title: 'Post 3',
-      image: 'post8.jpg',
-      description: 'Industria 4.0',
-      body: 'Meu terceiro post bla bla'
-    },
-    
-    {
-      id: 4,
-      title: 'Post 4',
-      image: 'post9.jpg',
-      description: 'Post qualquer',
-      body: 'Meu quarto post bla bla'
-    },
-  ]
+var postsFilePath = 'db/posts.json';
 
+var loadFilePosts = function(){
+  var fileData = fs.readFileSync(postsFilePath, 'utf8');
+  var posts = JSON.parse(fileData);
+}
   return posts;
+}
+
+var saveFilePost = function(newPost) { 
+  var posts = loadFilePosts();
+  posts.push(newPost);
+  saveFilePost(posts);
 }
 
 module.exports = {
   getPosts: getPosts
+  getPosts: getPosts,
+  savePost: savePost
 }
